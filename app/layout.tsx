ed21528613e8ed523210a774901e8a59cf282713
Nicholas Yang <58143649+NicholasY2509@users.noreply.github.com>
2026-05-21 08:09:38 +0700

    feat: Implement Speed Insight"

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,22 @@ import { Geist, Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
-import { AuthProvider } from "@/modules/auth/hooks/auth-context";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { Toaster } from "@/components/ui/sonner";
-import { QueryProvider } from "@/components/providers/query-provider";
-import NextTopLoader from 'nextjs-toploader';
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { cn } from "@/lib/utils"
+import { AuthProvider } from "@/modules/auth/hooks/auth-context"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { Toaster } from "@/components/ui/sonner"
+import { QueryProvider } from "@/components/providers/query-provider"
+import NextTopLoader from "nextjs-toploader"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
 
-import { NotificationProvider } from "@/components/providers/notification-provider";
+import { NotificationProvider } from "@/components/providers/notification-provider"
 
 export const metadata: Metadata = {
   title: {
@@ -34,14 +33,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-
   return (
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        inter.variable
+      )}
     >
       <body>
+        <SpeedInsights />
         <AuthProvider>
           <QueryProvider>
             <TooltipProvider>
