c005b6e1ec52a3b6af1f7a3229a1479100765ac0
Nicholas Yang <58143649+NicholasY2509@users.noreply.github.com>
2026-06-06 10:31:52 +0700

    feat: Modularize Icon Import

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import bundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@hugeicons/core-free-icons', '@hugeicons/react'],
+  }
+}
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer(nextConfig);
